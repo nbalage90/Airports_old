47eb60a79f7b19ca5c5e378a1013da6062cf9720
Balazs Nemeth <Balazs_Nemeth@epam.com>
2019-05-15 15:13:22 +0200

    Perfect pattern

--- Airports/Airports/Program.cs
+++ Airports/Airports/Program.cs
@@ -29,12 +29,7 @@ namespace Airports
 
         static void TryTransormDatas()
         {
-            var pattern = "^[0-9]{1,4},(\".*\",){5}([-0-9]{1,3}(\\.[0-9]{0,6})?,){2}";
-
-            //var line = "1,\"Goroka\",\"Goroka\",\"Papua New Guinea\",\"GKA\",\"AYGA\",-6.081689,145.391881,5282,10,\"U\"";
-            //var line2 = "1489,\"Ferihegy\",\"Budapest\",\"Hungary\",\"BUD\",\"LHBP\",47.436933,19.255592,495,1,\"E\"";
-            //Console.WriteLine(Regex.Match(line, pattern).Success);
-            //Console.WriteLine(Regex.Match(line2, pattern).Success);
+            var pattern = "^[0-9]{1,4},(\".*\",){5}([-0-9]{1,4}(\\.[0-9]{0,})?,){2}";
 
             using (var stream = new FileStream(@"Datas\airports.dat", FileMode.Open))
             using (var reader = new StreamReader(stream))
